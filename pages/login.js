b452fe5489d1a5e6bf85f989b433e63e7051b02f
YsisC <ysisclongart@gmail.com>
2023-05-03 22:04:31 -0400

    css login with tailwind

---
+++ pages/login.js
@@ -0,0 +1,63 @@
+import Head from "next/head";
+import Layout from "../layout/layout";
+import Link from "next/link";
+import styles from "../styles/Form.module.css"
+
+export default function Login() {
+    return (
+        <Layout>
+            <Head>
+                <title>Login</title>
+            </Head>
+            <section className="w-3/4 mx-auto flex flex-col gap-10">
+                <div className="title ">
+                    <h1 className=" text-pink-700 text-4xl font-bold py-4">Login</h1>
+                    <p>Las notas son divertidas y necesarias</p>
+                </div>
+                {/* form */}
+                <form className="flex flex-col gap-5">
+                    <div className={styles.input_group}>
+                        <input
+                            type="email"
+                            name="email"
+                            placeholder="Email"
+                            className={styles.input_text}
+                        />
+                    </div>
+                    <div className={styles.input_group}>
+                        <input
+                            type="pasword"
+                            name="pasword"
+                            placeholder="Pasword"
+                            className={styles.input_text}
+                        />
+                    </div>
+                    {/* login button */}
+                    <div className="input-button ">
+                        <button
+                            className={styles.button}
+                            type="submit">Login</button>
+
+                    </div>
+                    <div className="input-button ">
+                        <button className={styles.button_custom} type="button">
+                            Sign in with Google
+                        </button>
+                    </div>
+                    <div className="input-button ">
+                        <button className={styles.button_custom} type="button">
+                            Sign in with Github
+                        </button>
+                    </div>
+
+
+                </form>
+                {/* bottom */}
+                <p className="text-center text-gray-600">
+                    don't have an account yet? <Link legacyBehavior href={'/register'}><a className="text-pink-700">Sign Up</a></Link>
+                </p>
+            </section>
+        </Layout>
+
+    )
+}
---
+++ pages/register.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+export default function Register(){
+    return(
+        <div>
+            <Head>
+                <title>Register</title>
+            </Head>
+        Register
+        </div>
+    )
+}
